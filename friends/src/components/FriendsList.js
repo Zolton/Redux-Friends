9a8ac3cd902a3ad529dd08dc261542eef54b45d2
Zolton <gmzesq@gmail.com>
2019-06-19 17:06:00 -0400

    Set up classes, mstp

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,19 @@
+import React from "react"
+import React from "react";
+import { connect } from "react-redux";
+
+export class FriendsList extends React.Component {
+    render() {
+        return (
+            <>
+            Hello from FriendsList
+            </>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    
+}
+
+export default connect (mapStateToProps, {})(FriendsList)
