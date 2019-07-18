8d9ac4675c0861e91fe8a1f1594313cff7c70e47
Zolton <gmzesq@gmail.com>
2019-07-18 17:52:20 -0400

    Token setting works, login works

--- friends/src/component/PrivateRoute.js
+++ friends/src/component/PrivateRoute.js
@@ -1,11 +1,11 @@
-// import React from "react";
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Link,
-//   Redirect
-// } from "react-router-dom";
-// import Protected from "./Protected";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import Protected from "./Protected";
 
 // const PrivateRoute = ({ component: Component, ...rest }) => (
 //   <Route
@@ -16,36 +16,17 @@
 //   />
 // );
 
-// const PrivateRoute = ({ component: Component, props, ...rest}) => (
-//   <Route to={props.path}
-//    // {...rest}
-//     render={props =>
-//       localStorage.getItem("token") ? (
-//         <Component {...props} />
-//       ) : (
-//         <Redirect to="/" />
-//       )
-//     }
-//   />
-// );
-
-// export default PrivateRoute;
-
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-
-const PrivateRoute = (props) => {
-  const Component = props.component;
-  return (
-    <Route to={props.path}
-      render={ownprops =>
-        localStorage.getItem('token') ? (
-          <Component {...ownprops} />
-        ) : (
-          <Redirect to="/login" />
-        )}
-    />
-  );
-};
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      localStorage.getItem("token") ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/" />
+      )
+    }
+  />
+);
 
 export default PrivateRoute;
--- friends/src/component/Protected.js
+++ friends/src/component/Protected.js
@@ -3,17 +3,19 @@ import { axiosWithAuth } from "./axiosWithAuth";
 
 function Protected() {
   const [friends, setFriends] = useState();
-  useEffect(
+  useEffect(() => {
     axiosWithAuth()
       .get("http://localhost:5000/api/friends")
       .then(res => {
         setFriends(res.data);
-      })
-  );
+      });
+  }, []);
 
   return (
     <>
       <div> Hello from Protected</div>
+      {console.log(friends)}
+      
     </>
   );
 }
