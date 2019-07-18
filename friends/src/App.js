ad05631bceed8cf42358da4c4912be5885fe8253
Zolton <gmzesq@gmail.com>
2019-07-18 14:41:16 -0400

    Initial push/pull

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,26 @@
-import React from "react";
-//import logo from './logo.svg';
-import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-//import {Login} from "./components/Login"
-import FriendsList from "./components/FriendsList";
-import Login from "./components/Login";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        Hello from App
-        <Router>
-          <Link to="/">Login</Link>
-          <Link to="/protected">Friends List</Link>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/protected" component={FriendsList} />
-        </Router>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
